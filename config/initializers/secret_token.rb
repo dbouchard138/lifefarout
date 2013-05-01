7db40bc1fd0e71c45d6e7529333227879cad0ffe
Dana Bouchard <{dbouchard}@lifefarout.com>
2013-05-01 13:09:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifefarout::Application.config.secret_token = '46b40024cef1f970cac169a4c8081989b0af83f8588b38e8f6d3c4e4c5cad59d2d3a43ee0aff62959d7e7f88c980b8bf2b06a5f791109effacfb44af28ee4518'
